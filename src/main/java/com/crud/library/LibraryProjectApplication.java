35dce1b5232a86ed824f570b9c749a49faf296b2
Adam Wojcicki <adamwojcicki1991@gmail.com>
2020-06-29 14:23:01 +0200

    Initial commit

---
+++ src/main/java/com/crud/library/LibraryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.crud.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryProjectApplication.class, args);
+    }
+
+}
